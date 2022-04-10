929648bff06dad8becfa6c07625819ba63551b12
Konstantin Weißenow <60698240+kWeissenow@users.noreply.github.com>
2022-04-10 14:26:43 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
 ##################################
-# ProtT5dst
+# EMBER2
 # Inference pipeline for 2D structure prediction using language models
 # Authors: Konstantin Weißenow, Michael Heinzinger
 ##################################
@@ -211,7 +211,7 @@ def create_arg_parser():
     parser.add_argument('-o', '--output', required=True, type=str, help="A path where predictions are stored")
 
     parser.add_argument('--t5_model', required=True, type=str, help='A path to a directory holding the checkpoint for the pre-trained ProtT5 model' )
-    parser.add_argument('--dst_model', required=True, type=str, help='A path to the checkpoint file for the ProtT5dst model')
+    parser.add_argument('--dst_model', required=True, type=str, help='A path to the checkpoint file for the EMBER2 model')
     parser.add_argument('--stride', type=int, default=16, help="Cropping stride to use for predictions. Smaller values need exponentially more computation time, but might be more accurate. Default: 16")
     parser.add_argument('--batch_size', type=int, default=200, help="Batch size used for inference. Set lower values if you run out of memory. Default: 200")
     parser.add_argument('--workers', type=int, default=0, help="Number of threads used for data loading. Default: 0")
